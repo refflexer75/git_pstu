dc3cde314222375fd6288ee53e45997b0c91d458
refflexer75 <103984048+refflexer75@users.noreply.github.com>
2023-12-18 05:26:53 +0500

    Add files via upload

---
+++ Half division method2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f(double x) {
+    return ((3 * sin(sqrt(x))) + 0.35 * x - 3.8);
+}
+
+int main() {
+    double a, b, e, x;
+    int k;
+    setlocale(LC_ALL, "Russian");
+    a = 2; b = 3; e = 0.00001;
+    cout << "a=" << a << endl;
+    cout << "b=" << b << endl;
+    cout << "e=" << e << endl;
+
+    if (f(a) * f(b) < 0) {
+        cout << "Условие сходимости выполнено" << endl;
+        k = 0;
+        while (fabs(f(x)) >= e) {
+            x = (a + b) / 2.0;
+            k = k + 1;
+            if (f(a) * f(x) < 0) {
+                b = x; // Fix: Update b to x
+            } else {
+                a = x; // Fix: Update a to x
+            }
+        }
+        cout << "Корень x=" << x << endl;
+        cout << "Количество итераций k=" << k << endl;
+    } else {
+        cout << "Условие сходимости не выполнено" << endl;
+    }
+
+    return 0;
+}
+
